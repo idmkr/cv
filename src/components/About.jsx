8c3fe8e9afcb73bc69f8be492f43954725993942
Korman Diana <korman.dia@gmail.com>
2024-12-22 14:06:15 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
   return (
     <section id="about">
-      <img src="./photo.jpg" alt="Диана Корман" />
+      <img src="img/photo.jpg" alt="Диана Корман" />
       <p>
         Привет! Я Frontend разработчик с опытом создания пользовательских интерфейсов и веб-приложений.
         Имею опыт работы с JavaScript, React и инструментами для UI/UX дизайна.
